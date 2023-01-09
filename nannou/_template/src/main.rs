6656ac5eb51043639f999c9733d61f4d98db9d3d
Rafael Beckel <rafaelbeckel@gmail.com>
2023-01-09 11:05:04 +0100

    [nannou _template]: handles window resizing and code cleanup

--- nannou/_template/src/main.rs
+++ nannou/_template/src/main.rs
@@ -2,30 +2,26 @@ use nannou::color::*;
 use nannou::prelude::*;
 
 struct Model {
+    _window: WindowId,
     window_size: Vec2,
     rectangle_size: f32,
     warm_palette: Vec<Hsv>,
     cool_palette: Vec<Hsv>,
     grid: Vec<Rect>,
-    margin: f32,
     space: f32,
     animation_phase: f32,
     rotation_animation_phase: f32,
 }
 
 fn main() {
-    nannou::app(model)
-        .update(update)
-        //.event(event) // @todo handle window resize
-        .simple_window(view)
-        .run();
+    nannou::app(model).update(update).run();
 }
 
 fn model(app: &App) -> Model {
+    let window = app.new_window().view(view).event(event).build().unwrap();
     let window_size = app.window_rect().wh();
-    let rectangle_size = 16.0; // Set this to the size of each square in pixels.
-    let margin = 16.0;
-    let space = 2.0;
+    let rectangle_size = 16.0; // Set this to the size of each square.
+    let space = 2.0; // Set this to the space between each square.
 
     let num_colors: i32 = 360;
     let mut warm_palette: Vec<Hsv> = (0..num_colors)
@@ -68,26 +64,47 @@ fn model(app: &App) -> Model {
         .collect();
 
     Model {
+        _window: window,
         window_size,
         rectangle_size,
         warm_palette,
         cool_palette,
         grid,
-        margin,
         space,
         animation_phase: 0.0,
         rotation_animation_phase: 0.0,
     }
 }
 
-// fn event(_app: &App, model: &mut Model, event: WindowEvent) {
-//     match event {
-//         Resized(size) => {
-//             model.window_size = size;
-//         }
-//         _ => (),
-//     }
-// }
+fn update_model(model: &mut Model) {
+    let window_size = model.window_size;
+    let rectangle_size = model.rectangle_size; // Set this to the size of each square in pixels.
+    let space = model.space;
+    let n_cols = (window_size.x / rectangle_size) as i32;
+    let n_rows = (window_size.y / rectangle_size) as i32;
+
+    let grid = (0..n_cols)
+        .flat_map(|col| {
+            (0..n_rows).map(move |row| {
+                let x = col as f32 * (rectangle_size + space) - (window_size.x / 2.0);
+                let y = row as f32 * (rectangle_size + space) - (window_size.y / 2.0);
+                Rect::from_xy_wh(Vec2::new(x, y), Vec2::new(rectangle_size, rectangle_size))
+            })
+        })
+        .collect();
+
+    model.grid = grid;
+}
+
+fn event(_app: &App, model: &mut Model, event: WindowEvent) {
+    match event {
+        Resized(size) => {
+            model.window_size = size;
+            update_model(model);
+        }
+        _ => (),
+    }
+}
 
 fn update(app: &App, model: &mut Model, _update: Update) {
     model.animation_phase = (app.time).sin() / 2.0 + 0.5;
