c8eb4a99b95ea081ccfe7767a9f98e0ba7e6b1e7
Rafael Beckel <rafaelbeckel@gmail.com>
2023-02-02 11:53:51 +0100

    vulkan: particle system 2 - wip simple compute shader

---
+++ artwork/generative/nannou/particle_system_2/src/compute.rs
@@ -0,0 +1,23 @@
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage};
+use vulkano::descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet};
+use vulkano::device::{Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo};
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::pipeline::{ComputePipeline, Pipeline, PipelineBindPoint};
+use vulkano::sync::{self, GpuFuture};
+
+fn main() {
+    buffer_content[index] *= 12;
+
+    let data_iter = 0..65536;
+    let data_buffer = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage {
+            storage_buffer: true,
+            ..Default::default()
+        },
+        false,
+        data_iter,
+    )
+    .expect("failed to create buffer");
+}
