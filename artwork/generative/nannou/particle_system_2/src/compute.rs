62c45cfbea9859583c337b5cd62b6d8784ec5922
Rafael Beckel <rafaelbeckel@gmail.com>
2023-02-03 04:24:01 +0100

    vulkan: particle system 2 - full compute shader example

--- artwork/generative/nannou/particle_system_2/src/compute.rs
+++ artwork/generative/nannou/particle_system_2/src/compute.rs
@@ -7,8 +7,42 @@ use vulkano::pipeline::{ComputePipeline, Pipeline, PipelineBindPoint};
 use vulkano::sync::{self, GpuFuture};
 
 fn main() {
-    buffer_content[index] *= 12;
+    let library = vulkano::VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    let instance =
+        Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
 
+    let physical = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate devices")
+        .next()
+        .expect("no devices available");
+
+    let queue_family_index = physical
+        .queue_family_properties()
+        .iter()
+        .enumerate()
+        .position(|(_, q)| q.queue_flags.graphics)
+        .expect("couldn't find a graphical queue family") as u32;
+
+    let (device, mut queues) = Device::new(
+        physical,
+        DeviceCreateInfo {
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            enabled_extensions: DeviceExtensions {
+                khr_storage_buffer_storage_class: true,
+                ..DeviceExtensions::empty()
+            },
+            ..Default::default()
+        },
+    )
+    .expect("failed to create device");
+
+    let queue = queues.next().unwrap();
+
+    // Introduction to compute operations
     let data_iter = 0..65536;
     let data_buffer = CpuAccessibleBuffer::from_iter(
         device.clone(),
@@ -20,4 +54,71 @@ fn main() {
         data_iter,
     )
     .expect("failed to create buffer");
+
+    // Compute pipelines
+    mod cs {
+        vulkano_shaders::shader! {
+            ty: "compute",
+            src: "
+#version 450
+layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+layout(set = 0, binding = 0) buffer Data {
+    uint data[];
+} buf;
+void main() {
+    uint idx = gl_GlobalInvocationID.x;
+    buf.data[idx] *= 12;
+}"
+        }
+    }
+
+    let shader = cs::load(device.clone()).expect("failed to create shader module");
+    let compute_pipeline = ComputePipeline::new(
+        device.clone(),
+        shader.entry_point("main").unwrap(),
+        &(),
+        None,
+        |_| {},
+    )
+    .expect("failed to create compute pipeline");
+
+    let layout = compute_pipeline.layout().set_layouts().get(0).unwrap();
+    let set = PersistentDescriptorSet::new(
+        layout.clone(),
+        [WriteDescriptorSet::buffer(0, data_buffer.clone())], // 0 is the binding
+    )
+    .unwrap();
+
+    let mut builder = AutoCommandBufferBuilder::primary(
+        device.clone(),
+        queue.queue_family_index(),
+        CommandBufferUsage::OneTimeSubmit,
+    )
+    .unwrap();
+    builder
+        .bind_pipeline_compute(compute_pipeline.clone())
+        .bind_descriptor_sets(
+            PipelineBindPoint::Compute,
+            compute_pipeline.layout().clone(),
+            0, // 0 is the index of our set
+            set,
+        )
+        .dispatch([1024, 1, 1])
+        .unwrap();
+    let command_buffer = builder.build().unwrap();
+
+    let future = sync::now(device.clone())
+        .then_execute(queue.clone(), command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+
+    future.wait(None).unwrap();
+
+    let content = data_buffer.read().unwrap();
+    for (n, val) in content.iter().enumerate() {
+        assert_eq!(*val, n as u32 * 12);
+    }
+
+    println!("Everything succeeded!");
 }
