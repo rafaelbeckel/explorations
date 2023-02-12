b0bfdac24a8f3d5b405160f9611a62e81d274e98
Rafael Beckel <rafaelbeckel@gmail.com>
2023-02-12 03:27:08 +0100

    nannou: particle system 2 - more explanations about graphics pipeline

--- artwork/generative/nannou/particle_system_2/src/bin/graphics.rs
+++ artwork/generative/nannou/particle_system_2/src/bin/graphics.rs
@@ -14,3 +14,31 @@
 // > Note: There is nothing that the graphics pipeline can do that a compute pipeline couldn't do.
 // > However the graphics pipeline is much more specialized and therefore much more optimized.
 // > Some parts of the graphics pipeline are generally handled by dedicated chips on the hardware.
+
+// The purpose of the graphics pipeline is to draw a certain shape on an image.
+// This shape can be as simple as a single triangle, or as complex as a mountain range.
+
+// In order to start a graphical operation (i.e. an operation that uses the graphics pipeline),
+// you will need the following elements:
+// - A graphics pipeline object that describes the way the GPU should behave,
+//     similar to the way a compute pipeline object describes a compute operation.
+// - One or multiple buffers containing the shape of the object we want to draw.
+// - A framebuffer object, which is a collection of images to write to.
+// - Just like compute pipelines, we can also pass descriptor sets (and push constants,
+//     which we haven't covered yet).
+
+// When you start a graphics operation, the GPU will start by executing a _vertex shader_
+// (that is part of the graphics pipeline object) on each vertex of the shape that you want to draw.
+// This first step will allow you to position the shape on the screen.
+
+// Then the GPU finds out which pixels of the target image are covered by the shape, and executes
+// a _fragment shader_ (also part of the graphics pipeline object) on each of these pixels.
+// This shader is used to determine what is the color of the shape for the given pixel is.
+
+// Finall, the GPU will merge this color with the color that already exists at this location.
+
+// The graphics pipeline object contains the vertex shader, the fragment shader, plus various
+// other options that allows one to further configure the behavior of the graphics card.
+
+// > Note: This explanation only covers the fundamentals of graphics pipelines.
+//   Graphics pipelines have tons of configurable options, plus additional optional shader stages.
