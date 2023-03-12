b78fa424735b8b09a63c63984ed8eaa8750462ea
Rafael Beckel <rafaelbeckel@gmail.com>
2023-03-12 20:20:03 +0100

    creates feedAndMultiply function

--- tutorials/solidity/zombie_game/zombie_feeding.sol
+++ tutorials/solidity/zombie_game/zombie_feeding.sol
@@ -3,4 +3,13 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "./zombie_factory.sol";
 
-contract ZombieFeeding is ZombieFactory {}
+contract ZombieFeeding is ZombieFactory {
+    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+        uint256 newDna = (myZombie.dna + _targetDna) / 2;
+
+        _createZombie("NoName", newDna);
+    }
+}
