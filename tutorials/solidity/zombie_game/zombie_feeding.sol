3922a137e3f2ee468b92b43c0be1fc9c450c368b
Rafael Beckel <rafaelbeckel@gmail.com>
2023-03-13 03:10:41 +0100

    let's make the zombies eat some kittens...

--- tutorials/solidity/zombie_game/zombie_feeding.sol
+++ tutorials/solidity/zombie_game/zombie_feeding.sol
@@ -3,7 +3,28 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "./zombie_factory.sol";
 
+interface KittyInterface {
+    function getKitty(uint256 _id)
+        external
+        view
+        returns (
+            bool isGestating,
+            bool isReady,
+            uint256 cooldownIndex,
+            uint256 nextActionAt,
+            uint256 siringWithId,
+            uint256 birthTime,
+            uint256 matronId,
+            uint256 sireId,
+            uint256 generation,
+            uint256 genes
+        );
+}
+
 contract ZombieFeeding is ZombieFactory {
+    address cryptoKittiesAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(cryptoKittiesAddress);
+
     function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
